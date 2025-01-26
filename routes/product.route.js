377cb26eab85a40cc97a7d94c33f211fb4480735
aayush jain <aayushjain1290@gmail.com>
2025-01-26 23:25:13 +0530

    chudys

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
+const path = require('path');
 // const { register,loginUser } = require('../controllers/user.js');
 const { createProduct,getAllProduct } = require('../controllers/product.js');
 const { getProduct } = require('../controllers/product.js');
